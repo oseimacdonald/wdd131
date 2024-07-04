8b49ac0e8931abe61ea4ccdc799708a44956c863
oseimacdonald <oseimacdonald@gmail.com>
2024-07-04 18:08:32 +0000

    a

---
+++ scripts/place.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const currentYear = new Date().getFullYear();
+    const lastModified = document.lastModified;
+    document.getElementById('currentYear').textContent = currentYear;
+    document.getElementById('lastModified').textContent = lastModified;
+
+    const temperature = 15; // Static value for temperature
+    const windSpeed = 10; // Static value for wind speed
+
+    function calculateWindChill(temp, speed) {
+        if (temp <= 10 && speed > 4.8) {
+            return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(2);
+        } else {
+            return "N/A";
+        }
+    }
+
+    const windChill = calculateWindChill(temperature, windSpeed);
+    document.getElementById('windChill').textContent = windChill;
+});
